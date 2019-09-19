a8d8e320bc254c174bcd4b8808b8475290f3189f
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-19 13:27:59 +0200

    created _form partial, new, show views

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,4 +4,52 @@ class CarsController < ApplicationController
     @cars = Car.all
 
   end
+
+  def show
+    @car = Car.find(params[:id])
+
+  end
+
+  def new
+    @car = Car.new
+
+  end
+
+  def create
+
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to car_path(@car), notice: 'car was created'
+      else
+      render :new
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.updte(car_params)
+
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+
+  end
+
+  private
+
+
+  def car_params
+    params.require(:car).permit(:name, :image, :category, :doors , :seats, :gearshift, :air, :lagguage, :pick_up, :start_date, :end_date)
+
+  end
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'creating cars'
+
+usuario = User.new(
+  email: 'juanako@me.com',
+  password:123456
+  )
+  usuario.save
+
+
+   coche = Car.new(
+    name: "ferrari",
+    category: "economico",
+    doors: 2,
+    seats: 4,
+    air: "false",
+    start_date: DateTime.strptime("09/01/2009 17:00", "%m/%d/%Y %H:%M"),
+    user: usuario,
+
+    end_date: DateTime.strptime("09/01/2009 19:00", "%m/%d/%Y %H:%M")
+
+
+    )
+   coche.save!
+
+puts "done"
