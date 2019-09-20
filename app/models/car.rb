f767383ab696c6de7d4937e0018bfbc3563bdeb1
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-20 16:04:17 +0200

    added basic validations on car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,13 +1,30 @@
 class Car < ApplicationRecord
   belongs_to :user
 
+  #validations
+  validates :name, presence: true
+  validates :category, inclusion: { in: %w(Económico SUV Monovolúmenes),
+    message: "%{value} is not a valid category" }
+  validates :doors, inclusion: { in: 2...5 }
+  validates :seats, inclusion: { in: 2...5 }
+  validates :gearshift, inclusion: { in: %W(manual automatic), message: "%{value} is not a valid gearshift" }
+  validates :air, presence: true # maybe I have to check this out
+  validates :lagguage, presence: true
+  validates :pick_up, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+
+
+
+
 
 
 
 
    extend TimeSplitter::Accessors
   split_accessor :start_date
-  #split_accessor :start_date, date_format: "%D", time_format: "%I:%M%p"
+
 
 
 end
