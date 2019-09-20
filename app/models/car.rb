e602835fd05fe1921066dbe5354531a8be07ee49
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-20 17:26:06 +0200

    added form for photo and carriawera gem

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,8 +5,8 @@ class Car < ApplicationRecord
   validates :name, presence: true
   validates :category, inclusion: { in: %w(Económico SUV Monovolúmenes),
     message: "%{value} is not a valid category" }
-  validates :doors, inclusion: { in: 2...5 }
-  validates :seats, inclusion: { in: 2...5 }
+  validates :doors, inclusion: { in: 2..5 }
+  validates :seats, inclusion: { in: 2..5 }
   validates :gearshift, inclusion: { in: %W(manual automatic), message: "%{value} is not a valid gearshift" }
   validates :air, presence: true # maybe I have to check this out
   validates :lagguage, presence: true
@@ -14,6 +14,8 @@ class Car < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
 
+ #photo uploader
+  mount_uploader :image, PhotoUploader
 
 
 
