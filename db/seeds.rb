71ac0a8d278811dd90ede6f1f00b507912c94509
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-22 19:03:06 +0200

    added humanized_money

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts 'creating cars'
-user = User.create!(email: 'juajuan@meme.com', password: 123456)
+user = User.create!(email: 'juajan@meme.com', password: 123456)
 
    coche = Car.new(
     name: "lamborgini",
@@ -14,7 +14,8 @@ user = User.create!(email: 'juajuan@meme.com', password: 123456)
     user: user,
     sku: 'original-lambo',
     image: 'https://images.unsplash.com/photo-1517994112540-009c47ea476b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=781&q=80',
-    end_date: DateTime.strptime("09/01/2009 19:00", "%m/%d/%Y %H:%M")
+    end_date: DateTime.strptime("09/01/2009 19:00", "%m/%d/%Y %H:%M"),
+    price: 20.000
 
 
     )
