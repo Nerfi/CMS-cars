2b768f8f9e69b503acfa5623940ff5943f06d3da
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-22 17:02:38 +0200

    added sku to cars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_094216) do
+ActiveRecord::Schema.define(version: 2019_09_22_150053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_09_19_094216) do
     t.datetime "updated_at", null: false
     t.datetime "start_date"
     t.datetime "end_date"
+    t.string "sku"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
