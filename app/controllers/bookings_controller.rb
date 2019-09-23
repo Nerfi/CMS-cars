638c8debdbea489b6bbb64876c7d908c5f480346
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-23 23:54:05 +0200

    not working link_to Mis reservas

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
    skip_before_action :authenticate_user!, only: :show
+
   def create
     #raise
     car = Car.find(params[:car_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :bookings, only: [:show, :create] do
       resources :payments, only: :new
   end
 
+  resources :users, only: :show do
+    resources :bookings, only: :show
+  end
+
   root to: 'pages#home'
   resources :cars
-    mount StripeEvent::Engine, at: '/stripe-webhooks'
+
+
+  #added address from ngrok
+    mount StripeEvent::Engine, at: 'https://localhost:3000/stripe-webhooks'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
