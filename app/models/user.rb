cd0e35c87e6d296baa1062b2a9418f2493b987ae
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-25 14:31:10 +0200

    adding desing to show page cars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         #for API
   acts_as_token_authenticatable
 
 end
